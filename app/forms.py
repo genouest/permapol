87eabcaaff4ad42bdda697278fd4c773019a2e35
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2021-06-03 12:30:15 +0200

    validate group names

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import current_app
 
 from flask_wtf import FlaskForm
@@ -10,6 +12,10 @@ class CreateGroupForm(FlaskForm):
     group_name = StringField("Group name", validators=[DataRequired()])
 
     def validate_group_name(form, field):
+
+        if not re.match('^[a-zA-Z0-9_]+$', field.data):
+            raise ValidationError("Use only letters, digits or underscore")
+
         wa = current_app.config["APOLLO_INSTANCE"]
         if wa.groups.get_groups(field.data):
             raise ValidationError("This group name is already in use")
